c51c6d71c25d2fde3b3beaedb74124d74ee0ea97
Siddheshbolke <siddhesh13bolke@gmail.com>
2026-03-18 07:17:16 +0530

    first commit

---
+++ Frontend/src/components/creator/Creator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components_lite/Navbar'
+import amreshsir from './amreshsir.jpg'; // Import the local image
+import ankit from './Ankit.jpg';
+import ritik from './ritik.jpg';
+import gaurav from './gaurav.jpg';
+
+const Creator = () => {
+  return (
+    <div>
+      
+    </div>
+    
+    
+  )
+}
+
+export default Creator
