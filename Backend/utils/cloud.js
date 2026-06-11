713ea76ce353fc44534294dbdff7055486aed64a
Siddhesh  Bolke <123184343+Siddheshbolke@users.noreply.github.com>
2026-06-11 16:34:35 +0530

    Update cloud.js

--- Backend/utils/cloud.js
+++ Backend/utils/cloud.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_,
+  api_key: process.env.CLOUD_API,
   api_secret: process.env.API_SECRET,
 });
 
